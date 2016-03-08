323252be32abe8bd5d7126443da8a5cc005a5b16
meeksfred <maxfred0025@gmail.com>
2016-03-07 19:53:38 -0800

    moved things around, flushed out raw data

---
+++ scripts/portfolioArticles.js
@@ -0,0 +1,34 @@
+var theData = [
+  {
+    title: 'Code 201 Final Project',
+    category: 'projects',
+    author: 'Max Friedrichsen, Regan ONeill, Gareth Couch',
+    authorUrl: 'https://github.com/meeksfred/final-project',
+    publishedOn: '2015-12-17',
+    body: '<p>This was the final project that Regan, Gareth and I completed for our Code 201 class. Its a To-Do app for the Seattle Area where you can select a Seattle neighborhood, and then be presented with various restaurants/bars/parks/attractions within that specific neighborhood. You can also refine the search by selecting a price range.</p>'
+  },
+  {
+    title: 'Kitten Cuteness Tracker',
+    category: 'projects',
+    author: 'Max Friedrichsen',
+    authorUrl: 'https://github.com/meeksfred/kitty-tracker',
+    publishedOn: '2015-12-21',
+    body: '<p>This is a Kitten Cutness Tracking web app. There are 14 pictures of kittens in various poses, and you are to determine which is the cutest. This app will track how many times each kitten has been voted for, as well as visually represent those totals in a bar graph.</p>'
+  },
+  {
+    title: 'The Donut Shop',
+    category: 'projects',
+    author: 'Max Friedrichsen',
+    authorUrl: 'https://github.com/meeksfred/donut_shop',
+    publishedOn: '2015-12-22',
+    body: '<p>An app which tracks hypothetical donut sales at Top Pot locations in the surrounding Seattle area. Sales numbers are based on randomly generated amounts of customers per hour, as well as donuts sold per customer. Everything is presented in a generated table, and offers the chance for users to create their own Top Pot Doughnuts locations.</p>'
+  },
+  {
+    title: 'Maxs Guessing Game',
+    category: 'projects',
+    author: 'Max Friedrichsen',
+    authorUrl: 'https://github.com/meeksfred/guessing-game',
+    publishedOn: '2015-11-12',
+    body: '<p>A simple guessing game about myself. Wrong answers will be met with frowny faces and red lines through their answers, while correct answers will be met with smiley faces and green lines through their answers.</p>'
+  }
+];
