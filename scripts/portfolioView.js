9c8901fb5244ef3ba3aba52117072db16dd6ec15
meeksfred <maxfred0025@gmail.com>
2016-03-11 13:40:11 -0800

    Marcus and Gurpreet comments/changes

--- scripts/portfolioView.js
+++ scripts/portfolioView.js
@@ -3,7 +3,8 @@ var projectView = {};
 projectView.mainNav = function() {
   $('.head-nav').on('click', 'li', function () {
     $('.tab-content').hide();
-    $('#' + $(this).data('content')).show();
+    // $('#' + $(this).data('content')).show();
+    $('#' + $(this).data('content')).fadeIn(); //I recommended that Max use a .fadeIn() method, to make the change between single tabs seem smoother.
   });
 
   $('.head-nav .tab:first').click();
